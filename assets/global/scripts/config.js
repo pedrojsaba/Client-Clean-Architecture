ecb308077e6984141b477c26aae3b89cfa677037
Pedro J. Saba <pedrojsaba@gmail.com>
2017-11-18 02:29:57 -0500

    first commit

---
+++ assets/global/scripts/config.js
@@ -0,0 +1,9 @@
+/**
+Core script to handle the entire theme and core functions
+**/
+var Config = {
+    Service: 'http://net-clean-architecture.apphb.com/',
+    BaseUrl: '/',
+    NotAuthorized: 'No existe una cuenta activa',
+    TransferOk: 'Se realizo la transferencia'
+};
